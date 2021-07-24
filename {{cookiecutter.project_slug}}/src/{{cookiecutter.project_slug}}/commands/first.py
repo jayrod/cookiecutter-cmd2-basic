2b57ff363c3791bcfa209dc390a5b12599ed5341
Your Name <you@example.com>
2021-07-24 12:58:25 -0400

    Added sample parameter

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/commands/first.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/commands/first.py
@@ -1,4 +1,6 @@
-from cmd2 import with_default_category, CommandSet, Statement
+from cmd2 import with_default_category, CommandSet, Statement, Cmd2ArgumentParser, with_argparser
+    
+    
 
 @with_default_category('Other Commands')
 class FirstCommandSet(CommandSet):
@@ -9,5 +11,13 @@ class FirstCommandSet(CommandSet):
     def do_hello(self, _: Statement):
         self._cmd.poutput('Hello')
 
-    def do_world(self, _: Statement):
-        self._cmd.poutput('World')
+
+    parser = Cmd2ArgumentParser(description="CHANGE ME")
+    parser.add_argument(
+        "text", choices_provider=_get_aliases, help="HELP TEXT"
+    )
+
+    @with_argparser(parser)
+    def do_world(self, parms: Statement):
+
+        self._cmd.poutput(f'{parms.text}')
